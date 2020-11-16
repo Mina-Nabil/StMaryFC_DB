2b8a6634ca17c7a897cd4182b0ebff7cf8ad7874
Mina Nabil <mina9492@hotmail.com>
2020-11-16 22:08:36 +0200

    Adding user image url

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -255,7 +255,7 @@ class ApiController extends Controller
 
     private function adjustImageUrl($users){
         foreach($users as $key => $user){
-            $user->USIM_URL = url(Storage::url($user->USIM_URL));
+            $user->USIM_URL = $user->USIM_URL ? url(Storage::url($user->USIM_URL)) : NULL;
         }
     }
 }
