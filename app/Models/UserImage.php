4b9189404ff03532826e05703849160e252de975
Mina Nabil <mina9492@hotmail.com>
2020-12-16 12:22:39 +0200

    decreasing quality

--- app/Models/UserImage.php
+++ app/Models/UserImage.php
@@ -34,22 +34,27 @@ class UserImage extends Model
     {
         $quality = 10;
         // if (!$this->USIM_CMPS) {
-            $ext = last(explode('.', $this->USIM_URL));
-            $fileNoExt = str_replace('.' . $ext, '', $this->USIM_URL);
-            $imagePath = public_path('storage/' . $this->USIM_URL);
-            $newImagePath = $fileNoExt . '_' . $quality . '.' . $ext;
-            if ($ext == 'png') {
-                $image = imagecreatefrompng($imagePath);
-                imagejpeg($image, $newImagePath, $quality);
-            } else if ($ext == 'jpg' || $ext == 'jpeg') {
-                $image = self::imagecreatefromjpegexif($imagePath);
-                imagejpeg($image, $newImagePath, $quality);
-            }
-
+        $ext = last(explode('.', $this->USIM_URL));
+        $fileNoExt = str_replace('.' . $ext, '', $this->USIM_URL);
+        $imagePath = public_path('storage/' . $this->USIM_URL);
+        $newImagePath = $fileNoExt . '_' . $quality . '.' . $ext;
+        if ($ext == 'png') {
+            $image = imagecreatefrompng($imagePath);
+            imagejpeg($image, $newImagePath, $quality);
             $this->USIM_CMPS = 1;
             $this->USIM_URL = $newImagePath;
             $this->save();
             unlink($imagePath);
+        } else if ($ext == 'jpg' || $ext == 'jpeg') {
+            $image = self::imagecreatefromjpegexif($imagePath);
+            imagejpeg($image, $newImagePath, $quality);
+            $this->USIM_CMPS = 1;
+            $this->USIM_URL = $newImagePath;
+            $this->save();
+            unlink($imagePath);
+        }
+
+
         // }
     }
 
