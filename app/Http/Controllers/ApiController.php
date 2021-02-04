3e05d555901638ef7a3ff113ba0ed487560cd458
Mina Nabil <mina9492@hotmail.com>
2021-02-05 01:17:52 +0200

    Adding Overview API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -366,7 +366,7 @@ class ApiController extends Controller
         $user = User::findOrFail($request->userID);
         $payments =  $user->getLatestPayments($request->months);
         $attendance = $user->getOverviewAttendance($request->months);
-        return $this->getApiMessage(true, $payments);
+        return $this->getApiMessage(true, $attendance->merge($payments));
     }
 
     public function getUserPayments($id)
