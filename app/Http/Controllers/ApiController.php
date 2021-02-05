25bcf232b00e24866e446235222f95e30afe6ceb
Mina Nabil <mina9492@hotmail.com>
2021-02-05 07:44:42 +0200

    Adding Overview API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -90,7 +90,8 @@ class ApiController extends Controller
                 );
             }
   
-            $users = $users->get(["app_users.id", "USER_NAME", "GRUP_NAME", "USIM_URL", "isAttended", "paymentsDue"]);
+            // $users = $users->get(["app_users.id", "USER_NAME", "GRUP_NAME", "USIM_URL", "isAttended", "paymentsDue"]);
+            $users = $users->toSql();
             if ($users) {
                 $this->adjustImageUrl($users);
                 return $this->getApiMessage(true, $users);
