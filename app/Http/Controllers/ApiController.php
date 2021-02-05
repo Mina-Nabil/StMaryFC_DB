8daf3305d84721595366cf679311ef1634ae4447
Mina Nabil <mina9492@hotmail.com>
2021-02-05 08:37:49 +0200

    Adding Overview API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -541,6 +541,39 @@ class ApiController extends Controller
         }
         return $ret;
     }
+
+    private function getMonthName(int $month)
+    {
+        switch ($month) {
+
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                return null;
+        }
+    }
 }
 
 class ApiMessage
