9ccf0f80860c4921d2a741ff91845b9b7d2e00e1
Mina Nabil <mina9492@hotmail.com>
2021-02-06 21:03:11 +0200

    Search fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -86,7 +86,7 @@ class ApiController extends Controller
                 (Select COUNT(payments.id) from payments where PYMT_USER_ID = app_users.id and MONTH(PYMT_DATE) = MONTH(CURDATE())) as monthlyPayments');
             foreach ($arguments as $value) {
                 $users = $users->whereRaw(
-                    " ( GRUP_NAME LIKE '{$value}%' OR USER_NAME LIKE '%{$value}' OR USER_NAME LIKE '{$value}%' OR YEAR(USER_BDAY) = ? ) ",
+                    " ( GRUP_NAME LIKE '{$value}%' OR USER_NAME LIKE '%{$value}%' OR YEAR(USER_BDAY) = ? ) ",
                     [$value]
                 );
             }
