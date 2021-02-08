9b885b231ff7fb93500863ed87ea0dc5a0b328ad
Mina Nabil <mina9492@hotmail.com>
2021-02-09 00:27:50 +0200

    Adding Status to name

---
+++ app/Models/EventsAttendance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventsAttendance extends Model
+{
+    protected $table = "events_attendance";
+    public $timestamps = false;
+}
