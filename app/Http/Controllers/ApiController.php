ee3357af5841ff59e6768e81cb4c8d0182118983
Mina Nabil <mina9492@hotmail.com>
2021-02-09 01:23:35 +0200

    Adding Status to name

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Attendance;
 use App\Models\Event;
 use App\Models\EventPayment;
+use App\Models\EventsAttendance;
 use App\Models\Group;
 use App\Models\Payment;
 use App\Models\User;
