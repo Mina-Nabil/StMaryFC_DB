077762fcf8943f1e1a597a9e66c33a63568e612d
Mina Nabil <mina9492@hotmail.com>
2021-02-09 00:29:53 +0200

    Adding Status to name

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -417,7 +417,7 @@ class ApiController extends Controller
 
     public function getUserEventPayments($id)
     {
-        $payments = EventPayment::with('user:app_users.id,USER_NAME', 'event:events.id,EVNT_NAME', 'attendance:events_attendance.id,EVAT_STTS')->where('EVPY_USER_ID', $id)->get();
+        $payments = EventPayment::with('user:app_users.id,USER_NAME', 'event:events.id,EVNT_NAME', 'attendance:EVAT_STTS')->where('EVPY_USER_ID', $id)->get();
         if ($payments)
             return $this->getApiMessage(true, $this->makeLikePayment3shanMickeyBeh($payments));
         else
