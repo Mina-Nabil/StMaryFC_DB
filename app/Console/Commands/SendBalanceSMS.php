f28528f3234661c35d3d78a431f0b390c5161f20
Mina Nabil <mina9492@hotmail.com>
2023-11-06 16:48:50 +0200

    pushing sms command

---
+++ app/Console/Commands/SendBalanceSMS.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Attendance;
+use App\Models\BalancePayment;
+use App\Models\PlayersCatogory;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class MigrateBalance extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'balance:send_sms';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'send balance sms for the latest month';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $now = Carbon::now();
+        $latestBalancePayments = BalancePayment::whereDate('created_at', '>=', $now->format('Y-m-01'))->get();
+        /** @var BalancePayment */
+        foreach($latestBalancePayments as $pymt){
+            $pymt->sendSms();
+        }
+        return Command::SUCCESS;
+    }
+}
