c3e77d9d539224d4acf09368211da64e0b700bfc
Mina Nabil <mina9492@hotmail.com>
2024-08-27 14:18:56 +0300

    adding whatsapp

--- resources/views/components/datatable.blade.php
+++ resources/views/components/datatable.blade.php
@@ -128,7 +128,7 @@
                                         </td>
                                     @elseif(array_key_exists('sendwhatsappPayment', $att))
                                         <td><a target="_blank"
-                                                href="https://wa.me/{{ $item->app_user->USER_MOBN }}?text={{ urlencode($item->sendSms(true)) }}">Send</a>
+                                                href="https://wa.me/+2{{ $item->app_user->USER_MOBN }}?text={{ urlencode($item->sendSms(true)) }}">Send</a>
                                         </td>
                                     @elseif(array_key_exists('assetImg', $att))
                                         <td>
